90f7989fe40ba0b0befb777d8d4b2a4271577c36
luannv <luannv@fastgo.mobi>
2020-08-25 09:04:25 +0700

    update generator build

--- lib/assets_generator_builder.dart
+++ lib/assets_generator_builder.dart
@@ -4,4 +4,4 @@ import 'package:build/build.dart';
 /// A [Builder] that get the assets path from `pubspec.yaml` and generate
 /// a `r.dart` with `const` properties of assets path. See [AssetsBuilder]
 /// for more detail.
-Builder assetScannerBuilder(BuilderOptions builderOptions) => AssetsBuilder();
+Builder assetGeneratorBuilder(BuilderOptions builderOptions) => AssetsBuilder();
